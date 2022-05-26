9ded720dff76eb97b47fb8f32496669ab64dc102
Ramy M.Ahmed <85303564+xRamyx@users.noreply.github.com>
2022-05-27 00:05:38 +0200

    Added login functionality.

--- client/src/sample/Controller.java
+++ client/src/sample/Controller.java
@@ -10,9 +10,15 @@ public class Controller {
 
     @FXML
     private Button btnSend;
+    @FXML
+    private Button btnLogin;
 
     @FXML
     private TextField textField;
+    @FXML
+    private TextField userField;
+    @FXML
+    private TextField passField;
 
     @FXML
     private TextArea textArea;
@@ -29,5 +35,21 @@ public class Controller {
         textArea.appendText("Client: " + textField.getText() + "\n");
         textArea.appendText("Server: " + serverResponse.get("data") + "\n");
     }
-}
 
+    @FXML
+    void btnLogin_Handler(){
+
+        // sending json
+        SocketClient socketClient = SocketClient.getInstance();
+        JSONObject json = new JSONObject();
+        json.put("user", userField.getText());
+        json.put("pass", passField.getText());
+        String dist = "login";
+        JSONObject serverResponse = socketClient.socketSendReceiveJSON(json, dist);
+        if(serverResponse.get("access").equals("Granted")){
+            textArea.appendText("Access granted" + "\n");
+        }else{
+            textArea.appendText("Wrong credentials" + "\n");
+        }
+    }
+}
